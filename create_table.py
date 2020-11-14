ddbf619b1aa12e998bc3b8848e33bf05c2cbdeaf
otsubo2497 <otsubo2497@gmail.com>
2020-11-15 01:17:32 +0900

    membersテーブルの作成をPythonから実現できるようにした

---
+++ create_table.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+def main():
+    print('membersテーブルをつくるぞ！')
+
+    # DBに接続する
+    conn = sqlite3.connect('sample.db')
+
+    # 実行したいSQL文を用意する
+    sql = """
+    CREATE TABLE "members" (
+    "name"	TEXT,
+    "age"	INTEGER);
+    
+    
+    """  # TO DO: 後で書く
+
+    # SQL文を実行する
+    conn.execute(sql)
+    conn.commit()
+
+    # 接続を閉じる
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
